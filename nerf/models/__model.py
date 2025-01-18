7e619e25e7ac359152f278c31b094b991e634335
Lilian Schall <lilian.schall@epita.fr>
2025-01-18 13:05:59 +0100

    feat: added render method

--- nerf/models/__model.py
+++ nerf/models/__model.py
@@ -2,6 +2,7 @@ from .__embedder import Embedder
 from .__nerf import NeRF
 
 import torch
+import torch.nn.functional as F
 import os
 import typing as t
 
@@ -19,6 +20,7 @@ class Model:
     white_bkgd: bool
     raw_noise_std: float
     n_samples: int
+    lindisp: bool
 
     def __init__(
         self: "Model",
@@ -40,6 +42,7 @@ class Model:
         n_samples: int = 64,
         white_bkgd: bool = False,
         raw_noise_std: float = 0.0,
+        lindisp: bool = False,
     ):
         skips: list[int] = [4]
 
@@ -52,9 +55,10 @@ class Model:
         self.white_bkgd = white_bkgd
         self.raw_noise_std = raw_noise_std
         self.device = device
+        self.lindisp = lindisp
 
         if use_viewdirs and multires_views is not None:
-            embedder_views = Embedder(3, multires_views - 1, multires_views, True)
+            self.embedder_views = Embedder(3, multires_views - 1, multires_views, True)
 
         self.model = NeRF(
             D=net_depth,
@@ -104,75 +108,87 @@ class Model:
             if self.model_fine is not None:
                 self.model_fine.load_state_dict(checkpoint["model_fine_state_dict"])
 
-        def batchify(
-            self: "Model", embedded: torch.Tensor, netchunk: int
-        ) -> torch.Tensor:
-            return torch.cat(
-                [
-                    self.model(embedded[i : i + netchunk])
-                    for i in range(0, embedded.shape[0], netchunk)
-                ],
-                0,
+    def batchify(self: "Model", embedded: torch.Tensor, netchunk: int) -> torch.Tensor:
+        return torch.cat(
+            [
+                self.model(embedded[i : i + netchunk])
+                for i in range(0, embedded.shape[0], netchunk)
+            ],
+            0,
+        )
+
+    def run(
+        self: "Model",
+        inputs: torch.Tensor,
+        viewdirs: torch.Tensor | None,
+        netchunk: int | None = 1024 * 64,
+        use_fine: bool = False,
+    ) -> torch.Tensor:
+        inputs_flat: torch.Tensor = torch.reshape(inputs, (-1, inputs.shape[-1]))
+
+        embedded = self.embedder.embed(inputs_flat)
+
+        if viewdirs is not None and self.embedder_views is not None:
+            input_dirs = viewdirs[:, None].expand(inputs.shape)
+            input_dirs_flat = torch.reshape(input_dirs, (-1, input_dirs.shape[-1]))
+            embedded_dirs = self.embedder_views.embed(input_dirs_flat)
+            embedded = torch.cat([embedded, embedded_dirs], dim=-1)
+
+        if netchunk is None:
+            outputs_flat: torch.Tensor = (
+                self.model(embedded)
+                if not use_fine or self.model_fine is None
+                else self.model_fine(embedded)
             )
+        else:
+            outputs_flat: torch.Tensor = self.batchify(embedded, netchunk)
 
-        def run(
-            self: "Model",
-            inputs: torch.Tensor,
-            viewdirs: torch.Tensor | None,
-            netchunk: int | None = 1024 * 64,
-        ) -> torch.Tensor:
-            inputs_flat: torch.Tensor = torch.reshape(inputs, (-1, inputs.shape[-1]))
+        outputs: torch.Tensor = torch.reshape(
+            outputs_flat, list(inputs.shape[:-1]) + [outputs_flat.shape[-1]]
+        )
 
-            embedded = self.embedder.embed(inputs_flat)
+        return outputs
 
-            if viewdirs is not None and self.embedder_views is not None:
-                input_dirs = viewdirs[:, None].expand(inputs.shape)
-                input_dirs_flat = torch.reshape(input_dirs, (-1, input_dirs.shape[-1]))
-                embedded_dirs = self.embedder_views.embed(input_dirs_flat)
-                embedded = torch.cat([embedded, embedded_dirs], dim=-1)
+    def raw2outputs(
+        self: "Model", raw: torch.Tensor, z_vals: torch.Tensor, rays_d: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        raw2alpha: t.Callable = lambda raw, dists, act_fn=F.relu: 1.0 - torch.exp(
+            -act_fn(raw) * dists
+        )
 
-            if netchunk is None:
-                outputs_flat: torch.Tensor = self.model(embedded)
-            else:
-                outputs_flat: torch.Tensor = self.batchify(embedded)
+        dists: torch.Tensor = z_vals[..., 1:] - z_vals[..., :-1]
+        dists = torch.cat(
+            [dists, torch.Tensor([1e10]).expand(dists[..., :1].shape)], -1
+        )
 
-            outputs: torch.Tensor = torch.reshape(
-                outputs_flat, list(inputs.shape[:-1]) + [outputs_flat.shape[-1]]
-            )
+        dists = dists * torch.norm(rays_d[..., None, :], dim=-1)
 
-            return outputs
+        rgb: torch.Tensor = torch.sigmoid(raw[..., :3])
 
-        def raw2outputs(
-            self: "Model", raw: torch.Tensor, z_vals: torch.Tensor, rays_d: torch.Tensor
-        ) -> tuple[
-            torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor
-        ]:
-            raw2alpha: t.Callable = lambda raw, dists, act_fn=F.relu: 1.0 - torch.exp(-act_fn(raw) * dists)
+        noise: torch.Tensor | float = 0.0
 
-            dists: torch.Tensor = z_vals[..., 1:] - z_vals[..., :-1]
-            dists = torch.cat([dists, torch.Tensor([1e10]).expand(dists[..., :1].shape)], -1)
+        if self.raw_noise_std > 0.0:
+            noise = torch.randn(raw[..., 3].shape) * self.raw_noise_std
 
-            dists = dists * torch.norm(rays_d[..., None, :], dim=-1)
+        alpha: torch.Tensor = raw2alpha(raw[..., 3] + noise, dists)
 
-            rgb: torch.Tensor = torch.sigmoid(raw[..., :3])
+        weights: torch.Tensor = (
+            alpha
+            * torch.cumprod(
+                torch.cat([torch.ones((alpha.shape[0], 1)), 1.0 - alpha + 1e-10], -1),
+                -1,
+            )[:, :-1]
+        )
+        rgb_map: torch.Tensor = torch.sum(weights[..., None] * rgb, -2)
 
-            noise: torch.Tensor | float = 0.0
+        depth_map: torch.Tensor = torch.sum(weights * z_vals, -1)
+        disp_map: torch.Tensor = 1.0 / torch.max(
+            1e-10 * torch.ones_like(depth_map), depth_map / torch.sum(weights, -1)
+        )
 
-            if self.raw_noise_std > 0.0:
-                noise = torch.randn(raw[..., 3].shape) * self.raw_noise_std
+        acc_map: torch.Tensor = torch.sum(weights, -1)
 
+        if self.white_bkgd:
+            rgb_map = rgb_map + (1.0 - acc_map[..., None])
 
-            alpha: torch.Tensor = raw2alpha(raw[..., 3] + noise, dists)
-
-            weights: torch.Tensor = alpha * torch.cumprod(torch.cat([torch.ones((alpha.shape[0], 1)), 1.0 - alpha + 1e-10], -1), -1)[:, :-1]
-            rgb_map: torch.Tensor = torch.sum(weights[..., None] * rgb, -2)
-
-            depth_map: torch.Tensor = torch.sum(weights * z_vals, -1)
-            disp_map: torch.Tensor = 1.0 / torch.max(1e-10 * torch.ones_like(depth_map), depth_map / torch.sum(weights, -1))
-
-            acc_map: torch.Tensor = torch.sum(weights, -1)
-
-            if self.white_bkgd:
-                rgb_map = rgb_map + (1.0 - acc_map[..., None])
-
-            return rgb_map, disp_map, acc_map, weights, depth_map
+        return rgb_map, disp_map, acc_map, weights, depth_map
--- nerf/render/__renderer.py
+++ nerf/render/__renderer.py
@@ -1,26 +1,181 @@
 import torch
 import numpy as np
 
+from .__ray_generator import RayGenerator
+from nerf.models import Model
+
+
 class Renderer:
-    def __init__(self):
+    ray_generator: RayGenerator
+    model: Model
+
+    def __init__(self: "Renderer", model: Model):
+        self.ray_generator = RayGenerator()
+        self.model = model
         pass
 
     def render(
-        self,
+        self: "Renderer",
         H: int,
         W: int,
         K: np.ndarray,
-        chunk: int=1024*32,
-        rays: torch.Tensor | None = None,
+        rays: torch.Tensor,
+        chunk: int = 1024 * 32,
         c2w: torch.Tensor | None = None,
-        ndc:bool=True,
-        near:float=0.,
-        far:float=1.,
-        use_viewdirs:bool=False,
+        ndc: bool = True,
+        near: float = 0.0,
+        far: float = 1.0,
         c2w_staticcam=None,
-    ) -> None:
-        pass
+        retraw: bool = False,
+    ) -> tuple[list[torch.Tensor], dict[str, torch.Tensor]]:
 
+        if c2w is not None:
+            rays_o, rays_d = self.ray_generator.torch_rays(H, W, K, c2w)
+        else:
+            rays_o, rays_d = rays
+
+        viewdirs: torch.Tensor | None = None
+
+        if self.model.use_viewdirs:
+            viewdirs = rays_d
+            if c2w_staticcam is not None:
+                rays_o, rays_d = self.ray_generator.torch_rays(H, W, K, c2w_staticcam)
+
+            viewdirs = viewdirs / torch.norm(viewdirs, dim=-1, keepdim=True)
+            viewdirs = torch.reshape(viewdirs, [-1, 3]).float()  # type: ignore
+
+        sh = rays_d.shape
+
+        if ndc:
+            rays_o, rays_d = self.ray_generator.ndc_rays(
+                H, W, K[0][0], 1.0, rays_o, rays_d
+            )
+
+        rays_o: torch.Tensor = torch.reshape(rays_o, [-1, 3]).float()
+        rays_d: torch.Tensor = torch.reshape(rays_d, [-1, 3]).float()
+
+        near_tensor, far_tensor = near * torch.ones_like(
+            rays_d[..., :1]
+        ), far * torch.ones_like(rays_d[..., :1])
+
+        rays = torch.cat([rays_o, rays_d, near_tensor, far_tensor], -1)
+
+        if self.model.use_viewdirs and viewdirs is not None:
+            rays = torch.cat([rays, viewdirs], -1)
+
+        all_ret = self.batchify_rays(rays, chunk, retraw)
+
+        for k in all_ret:
+            k_sh = list(sh[:-1]) + list(all_ret[k].shape[1:])
+            all_ret[k] = torch.reshape(all_ret[k], k_sh)
+
+        k_extract = ["rgb_map", "disp_map", "acc_map"]
+        ret_list = [all_ret[k] for k in k_extract]
+        ret_dict = {k: all_ret[k] for k in all_ret if k not in k_extract}
+
+        return ret_list, ret_dict
+
+    def batchify_rays(
+        self: "Renderer", rays: torch.Tensor, chunk: int, retraw: bool
+    ) -> dict[str, torch.Tensor]:
+        """
+        Batchify rays to avoid memory issues.
+        """
+        all_ret = {}
+        for i in range(0, rays.shape[0], chunk):
+            ret = self.render_rays(rays[i : i + chunk], retraw)
+            for k in ret:
+                if k not in all_ret:
+                    all_ret[k] = []
+                all_ret[k].append(ret[k])
+
+        for k in all_ret:
+            all_ret[k] = torch.cat(all_ret[k], 0)
+
+        return all_ret
+
+    def render_rays(
+        self: "Renderer", rays: torch.Tensor, retraw: bool
+    ) -> dict[str, torch.Tensor]:
+
+        n_rays: int = rays.shape[0]
+        rays_o, rays_d = rays[:, 0:3], rays[:, 3:6]
+
+        viewdirs: torch.Tensor | None = rays[:, -3:] if rays.shape[-1] > 8 else None
+
+        bounds: torch.Tensor = torch.reshape(rays[..., 6:8], [-1, 1, 2])
+        near, far = bounds[..., 0], bounds[..., 1]
+
+        t_vals: torch.Tensor = torch.linspace(
+            0.0, 1.0, steps=self.model.n_samples, device=rays.device
+        )
+
+        if not self.model.lindisp:
+            z_vals: torch.Tensor = near * (1.0 - t_vals) + far * t_vals
+        else:
+            z_vals: torch.Tensor = 1.0 / (
+                1.0 / near * (1.0 - t_vals) + 1.0 / far * t_vals
+            )
+
+        z_vals = z_vals.expand([n_rays, self.model.n_samples])
+
+        if self.model.perturb > 0.0:
+            mids: torch.Tensor = 0.5 * (z_vals[..., 1:] + z_vals[..., :-1])
+            upper: torch.Tensor = torch.cat([mids, z_vals[..., -1:]], -1)
+            lower: torch.Tensor = torch.cat([z_vals[..., :1], mids], -1)
+            t_rand: torch.Tensor = torch.rand(z_vals.shape)
+
+            z_vals = lower + (upper - lower) * t_rand
+
+        pts: torch.Tensor = (
+            rays_o[..., None, :] + rays_d[..., None, :] * z_vals[..., :, None]
+        )
+
+        raw: torch.Tensor = self.model.run(pts, viewdirs)
+        rgb_map, disp_map, acc_map, weights, depth_map = self.model.raw2outputs(
+            raw, z_vals, rays_d
+        )
+
+        if self.model.n_importance > 0:
+            rgb_map_0, disp_map_0, acc_map_0 = rgb_map, disp_map, acc_map
+
+            z_vals_mid: torch.Tensor = 0.5 * (z_vals[..., 1:] + z_vals[..., :-1])
+            z_samples: torch.Tensor = self.ray_generator.sample_pdf(
+                z_vals_mid,
+                weights[..., 1:-1],
+                self.model.n_importance,
+                det=(self.model.perturb == 0.0),
+            )
+            z_samples = z_samples.detach()
+
+            z_vals, _ = torch.sort(torch.cat([z_vals, z_samples], -1), -1)
+            pts = rays_o[..., None, :] + rays_d[..., None, :] * z_vals[..., :, None]
+
+            raw = self.model.run(pts, viewdirs, use_fine=True)
+            rgb_map, disp_map, acc_map, weights, depth_map = self.model.raw2outputs(
+                raw, z_vals, rays_d
+            )
+
+        ret: dict[str, torch.Tensor] = {
+            "rgb_map": rgb_map,
+            "disp_map": disp_map,
+            "acc_map": acc_map,
+        }
+
+        if retraw:
+            ret["raw"] = raw
+
+        if self.model.n_importance > 0:
+            ret["rgb0"] = rgb_map_0 # type: ignore
+            ret["disp0"] = disp_map_0 # type: ignore
+            ret["acc0"] = acc_map_0 # type: ignore
+            ret["z_std"] = torch.std(z_samples, -1, unbiased=False) # type: ignore
+
+        for k in ret:
+            if torch.isnan(ret[k]).any() or torch.isinf(ret[k]).any():
+                print(f"NaN/inf in {k}")
+
+        return ret
 
     def render_path(
         self,
@@ -34,4 +189,3 @@ class Renderer:
         render_factor: int = 0,
     ) -> None:
         pass
-
